07fdc5d6637964939016278efe946cc23da5001a
MikheilMusaelyan <mishkamusaeliani@gmail.com>
2023-04-03 23:11:45 -0700

    drag nodes, display them

--- src/app/nodes.ts
+++ src/app/nodes.ts
@@ -16,7 +16,7 @@ export let childs: Node[] = []
     }
     
 
-    queue = []
+    let queue: Node[] = []
     let spliced = false
     if(children.length == 0) {
       children.push(node)
@@ -41,16 +41,16 @@ export let childs: Node[] = []
           // console.log('node is in child')
           if(children[i].children.length == 0) {
               children[i].children.push(node)
-              return
+              break
           }
           newNode(children[i].children, node)
-          return
+          break
         } 
 
         else if(node.start >= children[i].end && i == (children.length - 1)){
           // console.log('node is the biggest in the children and cant compare to next one')
           children.push(node)
-          return
+          break
         }
 
         else if(node.start <= children[i].start && node.end > children[i].start) {
@@ -65,17 +65,16 @@ export let childs: Node[] = []
             }
             
             if(node.children[node.children.length - 1].children.length > 0) {
-                addToQueue(node.end, node.children[node.children.length - 1])
-                const queueCopy = [...queue];
-                queueCopy.forEach(element => {
+                addToQueue(node.end, node.children[node.children.length - 1], queue)
+                queue.forEach(element => {
                   //????? children or childs
                    newNode(children, element);
                   //?????
                 });
                 // queue = [] 
-                return
+                break
             }
-            return
+            break
         }
         
         else if(node.start < children[i].start && node.end <= children[i].start) {
@@ -83,56 +82,78 @@ export let childs: Node[] = []
             if(spliced == false){
               children.splice(i, 0, node)
             }
-            return 
+            break 
         }
 
     }
 }
 
 
-export function addToQueue(node: number, child: Node) {
+export function addToQueue(node: number, child: Node, queue: Node[]) {
     let checkChildren: any[] = child.children
 
     // console.log('added to queue: ', checkChildren.slice())
 
     for (let i = 0; i < checkChildren.length; i++) {
-      
       if(node > checkChildren[i].start) {
-        // if(node >= checkChildren[i].end || checkChildren[i].children.length == 0){}
-        addToQueue(node, checkChildren[i])
+        if(node >= checkChildren[i].end || checkChildren[i].children.length == 0){
+          continue
+        }
+        addToQueue(node, checkChildren[i], queue)
       }
       else {
         let toPush = checkChildren.splice(i)
-        putInQueue(toPush)
+        putInQueue(toPush, queue)
         break
       }
     }
 }
 
-let queue: Node[] = []
 
-export function putInQueue(toPush: Node[]) {
+export function putInQueue(toPush: Node[], queue: Node[]) {
     for (let child of toPush) {
-      let childChildren = child.children.splice(0)
+      let childChildren = child.children.splice(0) // ???
       queue.push(child);
       if(childChildren.length > 0){
-        putInQueue(childChildren) 
+        putInQueue(childChildren, queue) 
       }  
     }
     
 }
 
-
-export function findNode(id: number, array: any[]): any {
+export function deleteNode(id: number, array: any[]): any {
+  let nodeQueue: Node[] = []
   for(let i = 0; i < array.length; i++) {
-    if(array[i]?.id == id) {
-      
+    if(array[i]?.id == id) { // find the element      
+      if(array[i].children.length > 0){ 
+        let childrenCopy = array[i].children.slice()
+        if(array[i+1]){
+          for(let j = 0; j < childrenCopy.length; j++) {
+            if(childrenCopy[j].end > array[i+1].start){
+              let addToSibling = childrenCopy.splice(j, 1)
+              array.splice(i, 1, ...childrenCopy)
+              putInQueue(addToSibling, nodeQueue)
+              nodeQueue.forEach((e: Node) => {
+                newNode(array, e)
+              })
+              return
+            }
+            console.log('did not happen br')
+          }
+        }
+        console.log('array[i+1] is not there')
+        // there's no next node so our children could not overlap anything
+        array.splice(i, 1, ...childrenCopy)
+        return
+      }
+      // node has no children so its children won't be deleted
+      array.splice(i, 1)
       return
     }
-    findNode(id, array[i].children)
+    deleteNode(id, array[i].children)
   }
 }
 
-setTimeout(() => {
-  findNode(26,childs)
-}, 300);
+
+
+
--- src/app/single-day/event/event.component.ts
+++ src/app/single-day/event/event.component.ts
@@ -6,5 +6,16 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./event.component.css']
 })
 export class EventComponent {
+  @Input('event') thisEvent: any
+  @Input('divList') divList: any
+  @Input('divWidth') width: any
 
+  ngOnInit() {
+    this.thisEvent.start = Math.ceil(this.thisEvent.start)
+    this.thisEvent.end = Math.ceil(this.thisEvent.end)
+  }
+
+  handleDropEvent(event: MouseEvent){
+
+  }
 }
