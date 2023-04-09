af48ea78fd99e32d05b9c2705715fb8bb762e218
MikheilMusaelyan <mishkamusaeliani@gmail.com>
2023-04-08 17:45:26 -0700

    fix calendar

--- src/app/year/year.component.ts
+++ src/app/year/year.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Host } from '@angular/core';
 
 @Component({
   selector: 'app-year',
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./year.component.css']
 })
 export class YearComponent {
+  date: Date = new Date();
 
+  month = this.date.getMonth()
+  year = this.date.getFullYear(); 
+  day = this.date.getDate()
+  hours = this.date.getHours()
+  minute = this.date.getMinutes();
+  second = this.date.getSeconds(); 
+  
+  monthNames: string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+  ngOnInit() {
+    setInterval(() => {
+      this.date = new Date();
+    }, 1000); // 
+    console.log(this.year, this.month, this.day, this.hours, this.minute, this.second)
+  }
+
+  ngOnDestroy() {
+    
+  }
 }
