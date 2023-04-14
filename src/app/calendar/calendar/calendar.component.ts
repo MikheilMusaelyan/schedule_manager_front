81620ae3b094d63ea0fac83ec0b91a4f861eae4a
MikheilMusaelyan <mishkamusaeliani@gmail.com>
2023-04-14 16:30:02 -0700

    differenciate today and selected date on calednar

--- src/app/calendar/calendar/calendar.component.ts
+++ src/app/calendar/calendar/calendar.component.ts
@@ -17,6 +17,8 @@ export class CalendarComponent {
   currentDay: number;
   daysInMonth: number;
   firstDayOfMonth: number;
+
+  realToday: Date = this.getDateWithoutHours(new Date()); // current date, no hours
   
   rows: any[] = [];
 
@@ -27,12 +29,8 @@ export class CalendarComponent {
   ) {
     this.todaySubscription = this.store.pipe(select(selectToday))
     .subscribe((today: Date) => {
-      this.today = today;
-      this.currentYear = today.getFullYear();
-      this.currentMonth = today.getMonth();
-      this.currentDay = today.getDate();
-      this.daysInMonth = new Date(this.currentYear, this.currentMonth + 1, 0).getDate();
-      this.firstDayOfMonth = new Date(this.currentYear, this.currentMonth, 1).getDay();
+      const fixedToday = this.getDateWithoutHours(today)
+      this.setNewDate(fixedToday)
       this.renderCalendar();
     }, error => console.error(error));
   }
@@ -58,9 +56,33 @@ export class CalendarComponent {
     }
   }
 
+  setNewDate(today: Date) {
+    this.today = today;
+    this.currentYear = today.getFullYear();
+    this.currentMonth = today.getMonth();
+    this.currentDay = today.getDate();
+    this.daysInMonth = new Date(this.currentYear, this.currentMonth + 1, 0).getDate();
+    this.firstDayOfMonth = new Date(this.currentYear, this.currentMonth, 1).getDay();
+  }
+
   ngOnDestroy(): void {
     this.todaySubscription.unsubscribe();
   }
 
+  gotToday() {
+    if(!this.today){
+      return false
+    } else {
+      return new Date(this.today).toString() == this.getDateWithoutHours(new Date()).toString()
+    }
+  }
+
+  getDateWithoutHours(date: Date) {
+    date.setHours(0)
+    date.setMinutes(0);
+    date.setSeconds(0);
+    date.setMilliseconds(0);
+    return date
+  }
 
 }
