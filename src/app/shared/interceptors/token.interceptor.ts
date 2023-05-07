03edd7506402908963bb87c96ed98ebeaaaf8434
MikheilMusaelyan <mishkamusaeliani@gmail.com>
2023-05-06 21:23:11 -0700

    ad

--- src/app/shared/interceptors/token.interceptor.ts
+++ src/app/shared/interceptors/token.interceptor.ts
@@ -8,7 +8,7 @@ export class AuthInterceptor implements HttpInterceptor {
     if (true) {
       request = request.clone({
         setHeaders: {
-          Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjgzNDEzMDE0LCJpYXQiOjE2ODM0MTIxMTQsImp0aSI6ImNiMjY2N2MyMzU5MTQzNTBiMDgxZmI2ZTUzMzMzNTUyIiwidXNlcl9pZCI6MjJ9.t5kEZ79ezDrceZ7Qmp2rWnXLvyHjj3g-UZoLSZlYWNI'
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjgzNDMzNzA3LCJpYXQiOjE2ODM0MzI4MDcsImp0aSI6IjE3NzA2NTJkYzY3MTQ3ZTc5ZGRiYWE3ZTUxOTk0NmY2IiwidXNlcl9pZCI6MjJ9.WZNMtqy4g2LN1kAGaxQ-zOxYh3mX1Y6-nM88Fdh5bQc'
         }
       });
     }
