6f1140e7d6d576ede376543f5872f95d54de2abe
MikheilMusaelyan <mishkamusaeliani@gmail.com>
2023-05-08 23:29:42 -0700

    finished search func

--- src/app/searchbar/searchbar.component.ts
+++ src/app/searchbar/searchbar.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { faSearch, faX, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { Subject, debounceTime, distinctUntilChanged, of, switchMap, tap } from 'rxjs';
@@ -13,68 +13,79 @@ export class SearchbarComponent {
   constructor(private http: HttpClient) { }
   search = faSearch
   remove = faXmark
-  searchSubject: Subject<any> = new Subject();
   // terms
-  selectedTerm: string = '';
+  selectedTerm: any[] = ['text', ''];
   loading: boolean = false;
+  @ViewChild('dateInput') dateInput: any;
   @ViewChild('input') input: any;
 
   form: FormGroup = new FormGroup({
-    start: new FormControl(''),
+    start: new FormControl(-1),
     name: new FormControl(''),
     date: new FormControl('')
   });
 
   ngOnInit(): void {
-   
   }
 
-  selectForm(term: string){
-    if(this.loading){
+  selectForm(term: any[]){
+    if(this.loading) return
+    this.selectedTerm = term;
+    this.input.nativeElement.focus()
+    setTimeout(() => {
+      if(term[1] == 'start' && this.form.value.start == -1){
+        this.input.nativeElement.value = 0
+      } else {
+        this.input.nativeElement.value = this.form.value[this.selectedTerm[1]]
+      }
+    }, 10);
+  }
+
+  onSearchInput(input: any) {
+    const value = input.target.value
+    if(this.selectedTerm[1] == 'start'){
+      if(value > 95){
+        this.input.nativeElement.value = Math.min(95, value)
+      } else if(value < 0) {
+        this.input.nativeElement.value = Math.max(0, value)
+      }
+      this.form.value.start = value;
       return
-    }
-    this.selectedTerm = term.trim();
-    this.input.nativeElement.value = `${this.selectedTerm}=${this.form.value[this.selectedTerm] || ''}`
-  }
-
-  onSearchInput(text: any): void {
-    let value: string = text.target.value.trim()
-    if(!value.startsWith(`${this.selectedTerm}=`) || this.loading){
-      this.selectedTerm = ''
+    } else if(this.selectedTerm[1] == 'name'){
+      if(value.length > 100){
+        this.input.nativeElement.value = ''
+      } 
+      this.form.value.name = value;
       return
-    }
-    const searchString = `${this.selectedTerm.trim()}=`
-    const startIndex = value.indexOf(searchString) + searchString.length;
-    const extractedString = value.substring(startIndex);
-    this.form.value[this.selectedTerm] = extractedString
-    console.log(this.form.value)
+    } 
+    this.form.value.date = value;
   }
 
-  clearTerm(term: string){
-    this.form.value[term.trim()] = '';
-    this.selectedTerm = ''
+  getType(){
+    return this.selectedTerm[0]
+  }
+
+  clearTerm(event: any, term: string) {
+    event.stopPropagation()
+    this.form.value[term] = '';
+    this.selectedTerm = ['text', '']
+    this.input.nativeElement.value = ''
   }
 
   onEnterKeyPressed(event: any){
     const newObject = {
       start: Number(this.form.value.start) || -1,
-      date: !isNaN(Date.parse(this.form.value.date)) ? new Date(this.form.value.date).toISOString().split('T')[0] : '-',
-      name: this.form.value.name || '-'
+      date: !isNaN(Date.parse(this.form.value.date)) ? this.form.value.date : '-',
+      name: this.form.value.name.length > 0 ? this.form.value.name : '-'
     }
-    if (event.keyCode === 13) {
-      this.form.reset()
-      this.loading = true
-      this.http.get(`http://127.0.0.1:8000/api/search/` + (newObject?.date?.trim()) + '/' + (newObject?.start) + '/' + (newObject?.name?.trim()) + '/')
-      .subscribe((results: any) => {
-        this.loading = false
-        console.log(results)
-      }, err => {
-        this.loading = false
-      });
-    }
-    
-  }
-  selectDate() {
-    // new Date().toISOString().split('T')[0],
+    this.form.reset()
+    this.loading = true
+    this.http.get(`http://127.0.0.1:8000/api/search/` + (newObject?.date?.trim()) + '/' + (newObject?.start) + '/' + (newObject?.name?.trim()) + '/')
+    .subscribe((results: any) => {
+      this.loading = false
+    }, err => {
+      this.loading = false
+    });
+  
   }
 }
